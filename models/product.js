31c8d2669f5acf9bbe7fb37618e25d75e511b769
Sahil-Soni1 <sonisahil334@gmail.com>
2022-04-14 22:34:48 +0530

    product model added

---
+++ models/product.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+require('mongoose-currency').loadType(mongoose);
+const Currency = mongoose.Types.Currency;
+
+const productSchema = new Schema({
+    name: {type: String, required: true},
+    description: String,
+    imgUrl: {type:String, required:true},
+    price: {type:Currency, required:true, min:0},
+    isVeg: {type: Boolean, required: true}
+}, { timestamps:true });
+
+module.exports = mongoose.model('Product', productSchema);
